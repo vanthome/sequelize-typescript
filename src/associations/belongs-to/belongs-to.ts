80131c0cbb073bc8200907ad3712d2631b5231a4
Thomas Hoppe <thomas.hoppe@n-fuse.co>
2022-07-30 17:38:58 +0200

    fix: BelongsTo vs NonBelongsToManyAssociationOptions

--- src/associations/belongs-to/belongs-to.ts
+++ src/associations/belongs-to/belongs-to.ts
@@ -2,7 +2,11 @@ import { BelongsToOptions } from 'sequelize';
 
 import { BelongsToAssociation } from './belongs-to-association';
 import { ModelClassGetter } from '../../model/shared/model-class-getter';
-import { addAssociation, getPreparedAssociationOptions } from '../shared/association-service';
+import {
+  addAssociation,
+  getPreparedAssociationOptions,
+  NonBelongsToManyAssociationOptions,
+} from '../shared/association-service';
 
 export function BelongsTo<TCreationAttributes, TModelAttributes>(
   associatedClassGetter: ModelClassGetter<TCreationAttributes, TModelAttributes>,
@@ -19,8 +23,18 @@ export function BelongsTo<TCreationAttributes, TModelAttributes>(
   optionsOrForeignKey?: string | BelongsToOptions<string, string>
 ): Function {
   return (target: any, propertyName: string) => {
-    const options: BelongsToOptions<string, string> =
+    const optionsOrig: NonBelongsToManyAssociationOptions =
       getPreparedAssociationOptions(optionsOrForeignKey);
+
+    if (!optionsOrig.inverse) {
+      optionsOrig.inverse = {};
+    }
+    if (optionsOrig.inverse.as === undefined) {
+      optionsOrig.inverse.as = 'hasOne';
+    }
+    const optionsAny: any = optionsOrig;
+
+    const options: BelongsToOptions<string, string> = optionsAny;
     if (!options.as) options.as = propertyName;
     addAssociation(target, new BelongsToAssociation(associatedClassGetter, options));
   };
