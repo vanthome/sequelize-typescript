685a3d29157a14a00840068cbb4915ddd21a8dc1
Thomas Hoppe <thomas.hoppe@n-fuse.co>
2022-09-27 13:56:59 +0200

    add compiled code

--- dist/associations/belongs-to/belongs-to.js
+++ dist/associations/belongs-to/belongs-to.js
@@ -6,14 +6,7 @@ const association_service_1 = require("../shared/association-service");
 function BelongsTo(associatedClassGetter, optionsOrForeignKey) {
     return (target, propertyName) => {
         const optionsOrig = (0, association_service_1.getPreparedAssociationOptions)(optionsOrForeignKey);
-        if (!optionsOrig.inverse) {
-            optionsOrig.inverse = {};
-        }
-        if (optionsOrig.inverse.as === undefined) {
-            optionsOrig.inverse.as = 'hasOne';
-        }
-        const optionsAny = optionsOrig;
-        const options = optionsAny;
+        const options = optionsOrig;
         if (!options.as)
             options.as = propertyName;
         (0, association_service_1.addAssociation)(target, new belongs_to_association_1.BelongsToAssociation(associatedClassGetter, options));
